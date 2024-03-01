309e72ded35d11f029a60edfe335ee6721c320ef
matt-pasek <mpasek@tm1.edu.pl>
2024-03-01 18:19:45 +0100

    added material icons library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.core.splashscreen)
+    implementation("androidx.compose.material:material-icons-extended:1.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
