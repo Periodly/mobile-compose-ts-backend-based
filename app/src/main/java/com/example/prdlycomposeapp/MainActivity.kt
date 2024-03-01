479cd4470b7eb066c1b659a4d362221aacaa996a
matt-pasek <mpasek@tm1.edu.pl>
2024-03-01 18:27:18 +0100

    login screen view

--- app/src/main/java/com/example/prdlycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/prdlycomposeapp/MainActivity.kt
@@ -3,13 +3,41 @@ package com.example.prdlycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.prdlycomposeapp.ui.theme.PRDLYComposeAppTheme
 
@@ -26,25 +54,105 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen() {
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var repeatPassword by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    var loginScreen by rememberSaveable { mutableStateOf(true) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PRDLYComposeAppTheme {
-        Greeting("Android")
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("PRDLY Compose App") })
+        }
+    ) {
+        p ->
+        Column(
+            modifier = Modifier
+                .padding(p)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = if (loginScreen) "Zaloguj się!" else "Zarejestruj się!",
+                        style = MaterialTheme.typography.displaySmall,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = username,
+                        onValueChange = { username = it },
+                        label = { Text("Nazwa użytkownika") },
+                        singleLine = true
+                    )
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Hasło") },
+                        singleLine = true,
+                        visualTransformation = if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        trailingIcon = {
+                            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                                val visibilityIcon =
+                                    if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                                val description = if (passwordHidden) "Show password" else "Hide password"
+                                Icon(imageVector = visibilityIcon, contentDescription = description)
+                            }
+                        }
+                    )
+                    if (!loginScreen) {
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = repeatPassword,
+                            onValueChange = { repeatPassword = it },
+                            label = { Text("Powtórz hasło") },
+                            singleLine = true,
+                            visualTransformation = if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            trailingIcon = {
+                                IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                                    val visibilityIcon =
+                                        if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                                    val description = if (passwordHidden) "Show password" else "Hide password"
+                                    Icon(imageVector = visibilityIcon, contentDescription = description)
+                                }
+                            }
+                        )
+                    }
+
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(if (loginScreen) "Zaloguj się" else "Zarejestruj się")
+                        }
+                        TextButton(
+                            onClick = { run { loginScreen = !loginScreen } }
+                        ) {
+                            Text(if (loginScreen) "Nie masz konta? Zarejestruj się" else "Zaloguj się")
+                        }
+                    }
+                    
+                }
+            }
+        }
     }
 }
