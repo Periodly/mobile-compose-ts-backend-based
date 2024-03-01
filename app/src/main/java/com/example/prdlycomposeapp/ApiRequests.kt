1798c0389718351c0d0e6803f7cce4d48c75d8d9
matt-pasek <mpasek@tm1.edu.pl>
2024-03-01 19:10:18 +0100

    api connections

--- app/src/main/java/com/example/prdlycomposeapp/ApiRequests.kt
+++ app/src/main/java/com/example/prdlycomposeapp/ApiRequests.kt
@@ -1,9 +1,14 @@
 package com.example.prdlycomposeapp
 
+import android.util.Log
+import androidx.activity.ComponentActivity
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApiRequests {
     companion object {
@@ -22,5 +27,20 @@ class ApiRequests {
             .build()
 
         private val api: ServerApi = retrofit.create(ServerApi::class.java)
+
+        fun login(user: User) {
+            api.login(user).enqueue(object : Callback<Token> {
+                override fun onResponse(call: Call<Token>, response: Response<Token>) {
+                    if (response.isSuccessful) {
+                        Log.d("ApiRequests", "onResponse: ${response.body()}")
+                        token.value = response.body()!!.token
+                    }
+                }
+
+                override fun onFailure(call: Call<Token>, t: Throwable) {
+                    Log.e("ApiRequests", "onFailure: ${t.message}")
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/example/prdlycomposeapp/ServerApi.kt
+++ app/src/main/java/com/example/prdlycomposeapp/ServerApi.kt
@@ -1,69 +1,60 @@
 package com.example.prdlycomposeapp
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Path
+
+data class User (
+    val id: Int?,
+    val username: String,
+    val password: String?,
+)
+
+data class Mood (
+    val id: Int?,
+    val userId: Int,
+    val mood: String,
+    val date: String
+)
+
+data class Token (
+    val token: String
+)
+
+data class Friend (
+    val id: Int?,
+    val userId: Int,
+    val friendId: Int
+)
+
+var token: MutableState<String> = mutableStateOf("")
 
 interface ServerApi {
     companion object {
         const val BASE_URL = "http://localhost:3000/api/"
     }
 
+    // user manipulation
+    @POST("session/login")
+    fun login(@Body user: User): Call<Token>
 
+    @POST("users")
+    fun register(@Body user: User): Call<Int>
 
-//    // products
-//    @GET("products")
-//    fun getProducts(): Call<MutableList<Product>>
-//
-//    @GET("products/{id}")
-//    fun getProduct(@Path("id") id: Int): Call<Product>
-//
-//    @POST("products")
-//    fun addProduct(@Body product: Product): Call<Int>
-//
-//    @PUT("products/{id}")
-//    fun updateProduct(@Path("id") id: Int, @Body product: Product): Call<Product>
-//
-//    @PATCH("products/{id}")
-//    fun patchProduct(@Path("id") id: Int, @Body product: Product): Call<Product>
-//
-//    @DELETE("products/{id}")
-//    fun deleteProduct(@Path("id") id: Int): Call<Int>
-//
-//    // users
-//    @GET("users")
-//    fun getUsers(): Call<MutableList<User>>
-//
-//    @GET("users/{id}")
-//    fun getUser(@Path("id") id: Int): Call<User>
-//
-//    @POST("users")
-//    fun addUser(@Body user: User): Call<Int>
-//
-//    @PUT("users/{id}")
-//    fun updateUser(@Path("id") id: Int, @Body user: User): Call<User>
-//
-//    @PATCH("users/{id}")
-//    fun patchUser(@Path("id") id: Int, @Body user: User): Call<User>
-//
-//    @DELETE("users/{id}")
-//    fun deleteUser(@Path("id") id: Int): Call<Int>
-//
-//    // orders
-//    @GET("orders")
-//    fun getOrders(): Call<MutableList<Order>>
-//
-//    @GET("orders/{id}")
-//    fun getOrderById(@Path("id") id: Int): Call<Order>
-//
-//    @POST("orders")
-//    fun addOrder(@Body order: Order): Call<Int>
-//
-//    @DELETE("orders/{id}")
-//    fun deleteOrder(@Path("id") id: Int): Call<Int>
+    // mood
+    @GET("moods")
+    fun getMoods(): Call<MutableList<Mood>>
+
+    @POST("moods")
+    fun addMood(@Body mood: Mood): Call<Int>
+
+    // friends
+    @GET("friends")
+    fun getFriends(): Call<MutableList<Friend>>
+
+    @POST("friends")
+    fun addFriend(@Body friend: Friend): Call<Int>
 }
